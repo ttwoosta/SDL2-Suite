7ac785c134ad362009a01df736dd92350160a9ab
Tu Tong <ttwoosta@gmail.com>
2021-01-30 20:07:15 -0500

    Print OpenGL version and driver version.

--- SDL2 Template/main.cpp
+++ SDL2 Template/main.cpp
@@ -45,6 +45,7 @@ protected:
     using seconds = std::chrono::duration<float>;
     void update(seconds frame, seconds total);
     void render() const;
+    void getGLVersion() const;
 private:
     sdl::Library SDL;
     Display output;
@@ -79,27 +80,49 @@ Project::Project(int w, int h)
     output {SDL, w, h}, 
     running {true},
     renderer { 
-        gfx::Shader{ gfx::Shader::Vertex, file_contents("plain_vertex.glsl") }, 
-        gfx::Shader{ gfx::Shader::Fragment, file_contents("plain_fragment.glsl") } 
+        // using default 2 shader vFlat and fFlat
+        gfx::Shader{ gfx::Shader::Vertex, shader::vFlat }, 
+        gfx::Shader{ gfx::Shader::Fragment, shader::fFlat } 
     },
     cam{ glm::perspective(1.0f, 4.0f / 3.0f, 1.0f, 200.0f) }
 {
-    renderer.attributes["position"] = 0;
+    // Program setup
+    // connect the position to channel 2 
+    renderer.attributes["position"] = 2;
+
+    // setup the color to 1
     renderer.attributes["color"] = 1;
+
+    // specify the value of a generic vertex attribute
+    // https://www.khronos.org/registry/OpenGL-Refpages/es2.0/xhtml/glVertexAttrib.xml
     glVertexAttrib4f(1, 1, 1, 1, 1);
 
-    cam << gfx::Vector3{ 0, 0, -5 };
+    // camera setup
+    // move the camera to a position (0,0,-5) 
+    // (translate the camera to -5 on Z-axis that back the camera out)
+    // move to the right and up 1 point
+    cam << gfx::Vector3{ 1, 1, -5 };
 
-    gfx::ArrayBuffer vs;
-    vs.Load(vs.StaticDraw, mesh_data);
+    
 
-    gfx::ElementArrayBuffer es;
-    es.Load(es.StaticDraw, face_data);
+    // copy mesh_data to Array buffer
+    // using StaticDraw option to tell openGL how to store the data
+    gfx::ArrayBuffer vertices;
+    vertices.Load(vertices.StaticDraw, mesh_data);
 
+    // copy face_data to Element Array buffer
+    gfx::ElementArrayBuffer edges;
+    edges.Load(edges.StaticDraw, face_data);
+
+    // Linking 2 buffers to Vertex array "mesh"
     mesh
-        << gfx::Vertex::ArrayAttribute<gfx::Float[3]>{renderer.attributes["position"], vs}
-        << es
+        << gfx::Vertex::ArrayAttribute<gfx::Float[3]>{renderer.attributes["position"], vertices}
+        << edges
         << gfx::Deactivate;
+
+    
+
+    this->getGLVersion();
 }
 
 int Project::operator()(const std::vector<std::string>& args)
@@ -121,25 +144,58 @@ void Project::update(seconds frame, seconds total)
     std::unordered_map<sdl::EventType, std::function<void(const sdl::Event&)>> responses;
     responses.emplace(sdl::EventType::Quit, [this](const sdl::Event&) { running = false; });
     SDL.ProcessEvents(responses);
+
 }
 
 void Project::render() const
 {
     constexpr gfx::Ushort start = 0;
     const gfx::Size count = face_data.size();
+
     output.Clear();
     cam
-        << renderer
-        << mesh
-        << std::make_pair(start, count);
+        << renderer // this is the program to use to draw 
+        << mesh     // draw instruction
+        << std::make_pair(start, count); // which peice out of the element array
+
     output.Refresh();
+
+
+}
+
+void Project::getGLVersion() const 
+{
+    using namespace std;
+
+    const GLubyte* p = glGetString(GL_VERSION);
+    std::cout << "OpenGL Version: " << p << std::endl;
+
+    const GLubyte* q = glGetString(GL_SHADING_LANGUAGE_VERSION);
+    std::cout << "GLSL Version: " << q << std::endl;
+
+    if (GLEW_ARB_vertex_array_object) {
+        printf("genVertexArrays supported\n");
+    }
+    else {
+        printf("genVertexArrays not supported\n");
+    }
+
+    if (GLEW_APPLE_vertex_array_object) {
+        printf("genVertexArraysAPPLE supported\n");
+    }
+    else {
+        printf("genVertexArraysAPPLE not supported\n");
+    }
 }
 
 int main(int argc, char* argv[])
 {
     using namespace std;
     try {
-        return Project{640, 480}(vector<string>{argv, argv + argc});
+
+        vector<string> param = vector<string>{ argv, argv + argc };
+        return Project{640, 480}(param);
+
     } catch (std::exception& e) {
         std::cerr << e.what() << '\n';
         return 1;
